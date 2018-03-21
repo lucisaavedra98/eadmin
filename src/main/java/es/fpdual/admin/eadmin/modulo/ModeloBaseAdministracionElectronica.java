b93bcc834a8ba387701d4151c95ff0f56e01aa43
formacion <formacion@mvformacion.localdomain>
2018-03-21 12:39:00 +0100

    Tercer Commit

---
+++ src/main/java/es/fpdual/admin/eadmin/modulo/ModeloBaseAdministracionElectronica.java
@@ -0,0 +1,55 @@
+package es.fpdual.admin.eadmin.modulo;
+
+import java.util.Date;
+
+public abstract class ModeloBaseAdministracionElectronica {
+	protected Integer codigo;
+	protected String nombre;
+	protected Date fechaCreacion;
+	protected Boolean publico;
+	
+	//Constructor
+	public ModeloBaseAdministracionElectronica(Integer codigo, String nombre, Date fechaCreacion, Boolean publico) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.fechaCreacion = fechaCreacion;
+		this.publico = publico;
+	}
+
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public Boolean getPublico() {
+		return publico;
+	}
+
+	@Override
+	public String toString() {
+		return "ModeloBaseAdministracionElectronica [codigo=" + codigo + ", nombre=" + nombre + ", fechaCreacion="
+				+ fechaCreacion + ", publico=" + publico + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		return codigo.hashCode() + nombre.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof ModeloBaseAdministracionElectronica) {
+			return codigo.equals(((ModeloBaseAdministracionElectronica) obj).getCodigo()) && nombre.equals(((ModeloBaseAdministracionElectronica) obj).getNombre());
+		}
+		return false;
+	}
+	
+}
---
+++ src/main/java/es/fpdual/admin/eadmin/modulo/ModeloBaseAdministracionElectronicaTest.java
@@ -0,0 +1,82 @@
+package es.fpdual.admin.eadmin.modulo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ModeloBaseAdministracionElectronicaTest {
+	
+	class ModeloBaseAdministracionElectronicaFake extends ModeloBaseAdministracionElectronica {
+		//Constructor
+		public ModeloBaseAdministracionElectronicaFake(Integer codigo, String nombre, Date fechaCreacion,
+				Boolean publico) {
+			super(codigo, nombre, fechaCreacion, publico);
+			// TODO Auto-generated constructor stub
+		}		
+		
+	}
+	
+	private static final Date FECHA_CREACION= new Date();
+	private static final String NOMBRE_DOCUMENTO = "nombre";
+	private static final boolean DOCUMENTO_PUBLICO= true;
+	private static final Integer CODIGO_DOCUMENTO= 1;
+	
+	//Inicializamos el objeto para poder modificarlo
+	ModeloBaseAdministracionElectronicaFake modelo;
+	
+	@Before
+	public void Inicializar() {
+		modelo =
+		new ModeloBaseAdministracionElectronicaFake (CODIGO_DOCUMENTO,NOMBRE_DOCUMENTO,FECHA_CREACION,
+				DOCUMENTO_PUBLICO);
+	}
+	
+	/*Método que compara parámetros por defecto guardados en constantes, con los parámetros que recoge el constructor */
+	@Test
+	public void DOCUMENTO_PUBLICO () {
+		assertEquals (CODIGO_DOCUMENTO,modelo.getCodigo());
+		assertEquals (NOMBRE_DOCUMENTO,modelo.getNombre());
+		assertEquals (FECHA_CREACION,modelo.getFechaCreacion());
+		assertEquals (DOCUMENTO_PUBLICO,modelo.getPublico());
+		
+	}
+	
+	@Test
+	public void deberiaDevolverTrueSiTienenIgualCodigo() {
+		final ModeloBaseAdministracionElectronicaFake modelo2 = new ModeloBaseAdministracionElectronicaFake (CODIGO_DOCUMENTO, NOMBRE_DOCUMENTO, null, null);
+		final Boolean resultado = modelo2.equals(modelo);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	public void deberiaDevolverFalseSiNoTienenIgualCodigo() {
+		final ModeloBaseAdministracionElectronicaFake modelo2 = new ModeloBaseAdministracionElectronicaFake(5, null, null, null);
+
+		final Boolean resultado = modelo2.equals(modelo);
+		assertFalse(resultado);
+	}
+
+	@Test
+	public void deberiaDevolverFalseSiNoEsUnDocumento() {
+		final boolean resultado = modelo.equals(new Date());
+		assertFalse(resultado);
+	}
+	
+	@Test
+	public void deberiaDevolverHasCodeDelCodigo () {
+		final ModeloBaseAdministracionElectronicaFake modelo3 = new ModeloBaseAdministracionElectronicaFake (CODIGO_DOCUMENTO, NOMBRE_DOCUMENTO, null, null );
+		final int resultado = modelo.hashCode();
+		assertEquals(resultado,modelo3.hashCode());
+	}
+	
+	@Test
+	public void deberiaDevolverString () {
+		assertNotNull(toString());
+	}
+}
